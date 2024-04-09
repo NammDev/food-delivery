0bceaf68195bfe3f85a38781137a895fb9b1cf99
Nam <namdeveloper.ca@gmail.com>
2024-04-09 10:09:18 +0700

    setup graphql & user type, entities

--- server/apps/users/src/users.module.ts
+++ server/apps/users/src/users.module.ts
@@ -1,10 +1,22 @@
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 import { Module } from '@nestjs/common';
-import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import {
+  ApolloFederationDriver,
+  ApolloFederationDriverConfig,
+} from '@nestjs/apollo';
 
 @Module({
-  imports: [],
-  controllers: [UsersController],
-  providers: [UsersService],
+  imports: [
+    GraphQLModule.forRoot<ApolloFederationDriverConfig>({
+      driver: ApolloFederationDriver,
+      autoSchemaFile: {
+        federation: 2,
+      },
+    }),
+  ],
+  providers: [UsersService, ConfigService, JwtService],
 })
 export class UsersModule {}
